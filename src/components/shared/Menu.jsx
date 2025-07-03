1ecf96b4289c78c7696193f4e575c1c69f05e31c
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-07-02 21:15:30 -0300

    agregamos logout

--- src/components/shared/Menu.jsx
+++ src/components/shared/Menu.jsx
@@ -1,8 +1,18 @@
 import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import logo from "../../assets/coffee_logo.png";
-import { NavLink, Link} from "react-router";
+import { NavLink, Link, useNavigate } from "react-router";
 
-const Menu = () => {
+const Menu = ({ usuarioAdmin, setUsuarioAdmin }) => {
+  console.log(usuarioAdmin)
+
+  const navegacion = useNavigate()
+
+  const logout = ()=>{
+    setUsuarioAdmin(false)
+    sessionStorage.removeItem('userKey')
+    navegacion('/')
+  }
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
@@ -17,11 +27,21 @@ const Menu = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <NavLink className="nav-link" to={'/'} >Inicio</NavLink>
-            <NavLink className="nav-link" to={'/administrador'} >Administrador</NavLink>
-            <NavLink className="nav-link " to={"/login"} >
-              Login
+            <NavLink className="nav-link" to={"/"}>
+              Inicio
             </NavLink>
+            {usuarioAdmin===true ? (
+              <>
+                <NavLink className="nav-link" to={"/administrador"}>
+                  Administrador
+                </NavLink>
+                <Button className="nav-link" onClick={logout}>Logout</Button>
+              </>
+            ) : (
+              <NavLink className="nav-link " to={"/login"}>
+                Login
+              </NavLink>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
