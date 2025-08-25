58978c8858b2b2b84416ede8b5eba383257ff2fe
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-08-25 20:10:20 -0300

    logramos borrar productos

--- src/components/pages/producto/ItemProducto.jsx
+++ src/components/pages/producto/ItemProducto.jsx
@@ -1,41 +1,42 @@
 import { Button } from "react-bootstrap";
 import { Link } from "react-router";
 import Swal from "sweetalert2";
+import { borrarProductoID } from "../../../helpers/queries";
 
-const ItemProducto = ({producto, fila, borrarProducto}) => {
-
-  const eliminarProducto = () =>{
+const ItemProducto = ({ producto, fila }) => {
+  const eliminarProducto = () => {
     Swal.fire({
-  title: "Eliminar producto",
-  text: "No puedes revertir este paso!",
-  icon: "warning",
-  showCancelButton: true,
-  confirmButtonColor: "#277a35ff",
-  cancelButtonColor: "#d33",
-  confirmButtonText: "Si, quiero eliminar!",
-  cancelButtonText: "Cancelar"
-}).then((result) => {
-  if (result.isConfirmed) {
-    if(borrarProducto(producto.id)){
-      Swal.fire({
-      title: "Producto eliminado!",
-      text: `El producto ${producto.nombreProducto} fue eliminado correctamente`,
-      icon: "success"
+      title: "Eliminar producto",
+      text: "No puedes revertir este paso!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#277a35ff",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, quiero eliminar!",
+      cancelButtonText: "Cancelar",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const respuesta = await borrarProductoID(producto._id);
+        if (respuesta.status===200) {
+          Swal.fire({
+            title: "Producto eliminado!",
+            text: `El producto ${producto.nombreProducto} fue eliminado correctamente`,
+            icon: "success",
+          });
+          //Luego debo actualizar la tabla
+        } else {
+          Swal.fire({
+            title: "Producto eliminado!",
+            text: `El producto ${producto.nombreProducto} no pudo ser eliminado`,
+            icon: "error",
+          });
+        }
+      }
     });
-    }
-    else{
-      Swal.fire({
-      title: "Producto eliminado!",
-      text: `El producto ${producto.nombreProducto} no pudo ser eliminado`,
-      icon: "error"
-    });
-    }
-  }
-});
-  }  
- 
- return (
-   <tr>
+  };
+
+  return (
+    <tr>
       <td className="text-center">{fila}</td>
       <td>{producto.nombreProducto}</td>
       <td className="text-end">${producto.precio}</td>
@@ -48,7 +49,10 @@ const ItemProducto = ({producto, fila, borrarProducto}) => {
       </td>
       <td>{producto.categoria}</td>
       <td className="text-center">
-        <Link className="me-lg-2 btn btn-warning" to={`/administrador/editar/${producto.id}`}>
+        <Link
+          className="me-lg-2 btn btn-warning"
+          to={`/administrador/editar/${producto.id}`}
+        >
           <i className="bi bi-pencil-square"></i>
         </Link>
         <Button variant="danger" onClick={eliminarProducto}>
