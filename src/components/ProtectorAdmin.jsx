64a40423545755352196e5fe12ffa718fb60574f
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-09-10 21:11:11 -0300

    resolvimos problema del login con el protector admin

--- src/components/ProtectorAdmin.jsx
+++ src/components/ProtectorAdmin.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router";
 
 const ProtectorAdmin = ({ isAdmin }) => {
   //si no soy admin
-  if (!isAdmin) {
+  if (!isAdmin.token) {
     return <Navigate to={"/"}></Navigate>;
   }
   //si soy admin
